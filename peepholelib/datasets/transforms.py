8e5db585b5429d8c57b31da3d58c110c4200a437
Leandro <leandro.desouzarosa@unibo.it>
2025-01-24 09:24:12 +0100

    minor

--- peepholelib/datasets/transforms.py
+++ peepholelib/datasets/transforms.py
@@ -1,6 +1,6 @@
 from torchvision import transforms
 
-vgg16_cifar10 = transforms.Compose([
+vgg16_cifar100 = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize((0.438, 0.418, 0.377), (0.300, 0.287, 0.294)) # CIFAR100
@@ -25,8 +25,8 @@ vgg16_cifar10_augumentations = transforms.Compose([
     ])
 
 mobile_netv2 = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]) 
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]) 
