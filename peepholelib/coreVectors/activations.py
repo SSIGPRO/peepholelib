fix normalization

peepholelib/coreVectors/activations.py
@@ -36,10 +36,8 @@ def get_activations(self, **kwargs):
     for ds_key in loaders:
         if verbose: print(f'\n ---- Getting data from {ds_key}\n')
         file_path = self.path/('activations.'+ds_key)
-        bs = loaders[ds_key].batch_size
-        
-        file_path = self.path/('activations.'+ds_key) 
         self._act_file_paths[ds_key] = file_path     
+        bs = loaders[ds_key].batch_size
 
         if file_path.exists():
             if verbose: print(f'File {file_path} exists. Loading from disk.')
@@ -145,12 +143,11 @@ def get_activations(self, **kwargs):
         for act_data in tqdm(act_dl, disable=not verbose, total=len(act_dl)):
             with torch.no_grad():
                 y_predicted = model(act_data['image'].to(device))
-                act_data['output'] = y_predicted
             
             # do not save predictions and results if it is already there
             if not has_pred:
                 predicted_labels = pred_fn(y_predicted)
-            
+                act_data['output'] = y_predicted
                 act_data['pred'] = predicted_labels
                 act_data['result'] = predicted_labels == act_data['label']
             

peepholelib/coreVectors/coreVectors.py
@@ -50,7 +50,7 @@ class CoreVectors():
         from_file = Path(kwargs['from_file']) if 'from_file' in kwargs else None
         wrt = kwargs['wrt'] if 'wrt' in kwargs else None
         to_file = Path(kwargs['to_file']) if 'to_file' in kwargs  else None
-        target_layers = kwargs['target_layers'] if 'target_layers' in kwargs  else None
+        target_layers = kwargs['target_layers'] if 'target_layers' in kwargs else None
 
         if wrt == None and from_file == None:
             raise RuntimeError(f'Specify `wrt` or `from_file`.')
@@ -62,26 +62,22 @@ class CoreVectors():
             if verbose: print(f'Computing normalization from {wrt}')
             means = self._corevds[wrt].mean(dim=0)
             stds = self._corevds[wrt].std(dim=0)
-            print(means['classifier.0'], stds['classifier.0'])
-            
+
         if target_layers != None:
             keys_to_pop = tuple(means.keys()-target_layers)
-
+            print('pop keys: ', keys_to_pop)
             for k in keys_to_pop:
-                means.pop(k,default=None)
-                stds.pop(k,default=None)
-   
+                means.pop(k, default=None)
+                stds.pop(k, default=None)
+
         for ds_key in self._corevds:
             if verbose: print(f'\n ---- Normalizing core vectors for {ds_key}\n')
             dl = DataLoader(self._corevds[ds_key], batch_size=bs, collate_fn=lambda x: x)
             
             for batch in tqdm(dl, disable=not verbose, total=len(dl)):
-                #print(batch['classifier.0'])
-                batch = (batch - means)/stds
-                #print(batch['classifier.0'])
+                for _k in means.keys():
+                    batch[_k] = (batch[_k]- means[_k])/stds[_k]
 
-            self._corevds[ds_key] = dl.dataset
-        
         if to_file != None:
             to_file.parent.mkdir(parents=True, exist_ok=True)
             torch.save((means, stds), to_file)
@@ -129,20 +125,20 @@ class CoreVectors():
 
         for ds_key in loaders:
             if verbose: print(f'\n ---- Getting data from {ds_key}\n')
-            file_path = self.path/(self.name.name+'.'+ds_key)
-            self._cvs_file_paths[ds_key] = file_path
             
-            if verbose: print(f'File {file_path} exists. Loading from disk.')
-            self._corevds[ds_key] = PersistentTensorDict.from_h5(file_path, mode='r')
+            self._cvs_file_paths[ds_key] = self.path/(self.name.name+'.'+ds_key)
+            self._act_file_paths[ds_key] = self.path/('activations.'+ds_key)
+
+            if verbose: print(f'Loading files {self._cvs_file_paths[ds_key]} and {self._act_file_paths[ds_key]} from disk. ')
+            self._corevds[ds_key] = PersistentTensorDict.from_h5(self._cvs_file_paths[ds_key], mode='r')
+            self._actds[ds_key] = PersistentTensorDict.from_h5(self._act_file_paths[ds_key], mode='r')
 
             self._n_samples[ds_key] = len(self._corevds[ds_key])
             if verbose: print('loaded n_samples: ', self._n_samples[ds_key])
        
         if norm_file != None:
             if verbose: print('Loading normalization info.')
-            means, stds = torch.load(norm_file)
-            self._norm_mean = means 
-            self._norm_std = stds
+            self._norm_mean, self._norm_std = torch.load(norm_file)
 
         return
     

peepholelib/coreVectors/get_coreVectors.py
@@ -3,7 +3,6 @@ from tqdm import tqdm
 
 # torch stuff
 import torch
-import torchvision
 from tensordict import PersistentTensorDict
 from tensordict import MemoryMappedTensor as MMT
 from torch.utils.data import DataLoader
@@ -84,16 +83,9 @@ def get_coreVectors(self, **kwargs):
 
         if verbose: print('Computing core vectors')
         
-        for lk in _layers_to_save:
-            
-            if verbose: print(f'\n ---- Getting corevectors for {lk}\n')
-            
-            for cvs_data, act_data in tqdm(zip(cvs_dl, act_dl), disable=not verbose, total=len(cvs_dl)):
-
-                ## TODO change this part in oreder to guarantee the possibility to choose between in and out activations
-                ## it could be usefule to choos the direction at the beginning and verify if that specific layer 
-                ## is present in either in or out activations
-                
+        if verbose: print(f'\n ---- Getting corevectors for {ds_key}\n')
+        for cvs_data, act_data in tqdm(zip(cvs_dl, act_dl), disable=not verbose, total=len(cvs_dl)):
+            for lk in _layers_to_save:
                 cvs_data[lk] = reduction_fns[lk](act_data[lk])
 
     return        

peepholelib/tests/norm_td.py
@@ -1,4 +1,4 @@
-from tensordict import TensorDict
+from tensordict import PersistentTensorDict as PTD
 from tensordict import MemoryMappedTensor as MMT
 import torch
 import sys
@@ -6,14 +6,10 @@ from torch.utils.data import DataLoader
 
 def p(td, bs):
     dl = DataLoader(dataset=td, batch_size=bs, collate_fn=lambda x: x)
-    print('\ndataloader: ', len(dl.dataset))
-    for d in dl:
-        print('batch')
-        for kk in d.keys():
-            print(kk, d[kk].contiguous())
-            if kk == 'b':
-                for k in d['b'].keys():
-                    print('b', k, d['b'][k].contiguous())
+    print('\n------: ')
+    for data in dl:
+        for kk in data.keys():
+            print(kk, data[kk].contiguous())
 
 if __name__ == '__main__':
     n = 5 
@@ -23,37 +19,32 @@ if __name__ == '__main__':
     n_dicts = 2 
     
     # seems like batch size is really important
-    td = TensorDict(batch_size=n, device=device)
+    td = PTD(filename='./banana', batch_size=[n], mode='w')
     for j in range(n_dicts):
         td['a%d'%j] = MMT.empty(shape=(n,ds))
-    
-    td = td.memmap_like('./banana')
-    # makes one dict for each input
-    for i in range(n):
-        d = {}
-        # create data
-        for j in range(2):
-            d['a%d'%j] = torch.rand((1, ds))
-        td[i] = TensorDict(d) 
+        td['a%d'%j] = torch.rand((n, ds))
     
     p(td, 5)
 
     m = td.mean(dim=0)
     s = td.std(dim=0)
-    print('mean: ', m)
-    print('std: ', s)
+    for k in m.keys():
+        print('mean: ', m[k])
+        print('std: ', s[k])
+    
     print('\n------------------------\n') 
     bs = 2
     dl = DataLoader(dataset=td, batch_size=bs, collate_fn=lambda x: x)
-    for bn, data in enumerate(dl):
-        n_in = len(data)
-        temp = (data-m)/s
-        td[bn*bs:bn*bs+n_in] = temp 
-
-    print('after norm')
+    for data in dl:
+        for k in m.keys():
+            print(f'{k} - before: ', data[k])
+            data[k] = (data[k]-m[k])/s[k]
+            print(f'{k} after: ', data[k])
+            
+    print('\nafter norm')
     p(td, 5)
 
     del td
     print('after loading')
-    td2 = TensorDict.load_memmap('./banana')
+    td2 = PTD.from_h5('./banana', mode='r')
     p(td2, 5)