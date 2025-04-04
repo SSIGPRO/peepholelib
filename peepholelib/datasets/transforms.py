46106eb57f61eac54aedd213015ad8dd03746d36
lorenzo <lorenzo.capelli@outlook.com>
2025-04-04 15:16:47 +0200

    minor changes

--- peepholelib/datasets/transforms.py
+++ peepholelib/datasets/transforms.py
@@ -30,3 +30,15 @@ mobile_netv2 = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]) 
+
+vgg16_imagenet = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+])
+
+vgg16_imagenet_augmentations = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.AutoAugment(policy=transforms.AutoAugmentPolicy.IMAGENET),
+    vgg16_imagenet
+])
