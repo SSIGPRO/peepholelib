c56b402d01abc3d9cf87c1964cda201b959ef44b
lorenzo <lorenzo.capelli@outlook.com>
2025-05-31 11:43:50 +0200

    trying to avoid using so much cpu

--- peepholelib/HeadSqueezing/HeadSqueezingDetector.py
+++ peepholelib/HeadSqueezing/HeadSqueezingDetector.py
@@ -20,6 +20,7 @@ class HeadSqueezingDetector():
         name = kwargs['name']
         self.target_module = kwargs['target_modules'] 
         sample_in = kwargs['sample_in']
+        mode = kwargs['mode'] if 'mode' in kwargs else 'first_k'
         self.k = kwargs['rank'] if 'rank' in kwargs else 300
         channel_wise = kwargs['channel_wise'] if 'channel_wise' in kwargs else True
         verbose = kwargs['verbose'] if 'verbose' in kwargs else False
@@ -37,62 +38,54 @@ class HeadSqueezingDetector():
                             )
 
         self._model.set_activations(save_input=True, save_output=False)
-    
-    def __call__(self, **kwargs):
-        image = kwargs['image']
-        device = kwargs['device']
-        mode = kwargs['mode'] if 'mode' in kwargs else 'first_k'
-        n_iter = kwargs['n_iter'] if 'n_iter' in kwargs else 1000
-        self.output_dict['ori'] = self._model(image.to(device))
-        act_data = self._model._acts['in_activations'][self.target_module]
-        n_act = act_data.shape[0]
+
         svd = self._model._svds[self.target_module]
-        
+
         if mode == 'first_k':
 
             Vh = svd['Vh'][:self.k].T
             U = svd['U'][:self.k]
             s = svd['s'][:self.k]
 
-            acts_flat = act_data.flatten(start_dim=1)
-            ones = torch.ones(n_act, 1, device=device)
-            _acts = torch.hstack((acts_flat, ones))
-            
-            A_ = Vh*s@U
-            
-            self.output_dict['squeezed'] = (A_.to(device).T@_acts.T).T
-            self.output = self.score_fn(self.output_dict['ori'], self.output_dict['squeezed'])
-
         elif mode == 'random':
+                    
+                    idx = torch.randperm(svd['Vh'].shape[0])[:self.k]
+                    
+                    Vh = svd['Vh'][idx].T
+                    U = svd['U'][idx]
+                    s = svd['s'][idx]
 
-            score = []
+        elif mode == 'last_k':
 
-            for i in range(n_iter):
-                idx = torch.randperm(svd['Vh'].shape[0])[:self.k]
-                
-                Vh = svd['Vh'][idx].T
-                U = svd['U'][idx]
-                s = svd['s'][idx]
-
-                acts_flat = act_data.flatten(start_dim=1)
-                ones = torch.ones(n_act, 1, device=device)
-                _acts = torch.hstack((acts_flat, ones))
-                
-                A_ = Vh*s@U
-                
-                self.output_dict['squeezed'] = (A_.to(device).T@_acts.T).T
-                sfn = self.score_fn(self.output_dict['ori'] - self.output_dict['squeezed'])/ self.score_fn(self.output_dict['ori'])
-                
-                score.append(sfn)
-            
-            self.output = torch.stack(score).std(dim=0)
-            
-    
-           
+                    Vh = svd['Vh'][-self.k:].T
+                    U = svd['U'][-self.k:]
+                    s = svd['s'][-self.k:]
         else:
-            raise ValueError(f"Unknown mode: {mode}. Use 'first_k' or 'random'.")
+
+            raise ValueError(f"Unknown mode: {mode}. Use 'first_k', 'random', or 'last_k'.")   
+        
+        self._A = Vh*s@U 
+        
+    def __call__(self, **kwargs):
+        image = kwargs['image']
+        device = kwargs['device']
+        mode = kwargs['mode'] if 'mode' in kwargs else 'first_k'
+        n_iter = kwargs['n_iter'] if 'n_iter' in kwargs else 10000
 
         
+        self.output_dict['ori'] = self._model(image.to(device))
+
+        act_data = self._model._acts['in_activations'][self.target_module].to(device)
+        n_act = act_data.shape[0]
+        
+        acts_flat = act_data.flatten(start_dim=1)
+        ones = torch.ones(n_act, 1, device=device)
+        _acts = torch.hstack((acts_flat, ones))
+            
+        self.output_dict['squeezed'] = (self._A.to(device).T@_acts.T).T
+            
+        self.output = self.score_fn(self.output_dict['ori'] - self.output_dict['squeezed'])
+    
         return self.output
 
         
