6fc3501a8017db2a42d6d8aa2629c50e07983a28
Leandro <leandro.desouzarosa@unibo.it>
2025-06-05 16:42:31 +0200

    wip

--- peepholelib/reduced_conv_svd.py
+++ peepholelib/reduced_conv_svd.py
@@ -12,7 +12,16 @@ from torch.nn.functional import pad
 from math import *
 
 
-def channel_conv(input_shape, layer, device='cpu', verbose=False, warns=True):
+def channel_conv(layer):
+    weight = layer.weight        
+    bias = layer.bias
+    #print('weights: ', weight)
+    uw = weight.flatten(start_dim=1, end_dim=-1)
+    #print('unrolled weights: ', uw)
+
+    return uw
+
+def unroll_image(img, layer):
     if not isinstance(layer, torch.nn.Conv2d):
         raise RuntimeError('Input layer should be a torch.nn.Conv2D one')
 
@@ -22,51 +31,69 @@ def channel_conv(input_shape, layer, device='cpu', verbose=False, warns=True):
     stride = layer.stride
     padding = layer.padding
     dilation = layer.dilation
+    pad_mode = layer.padding_mode
 
     if dilation != (1,1):
-        raise RuntimeError('This functions does not account for dilation, if you extendent it, please send us a PR ;).')
-    
-    if padding != (0, 0):
-        input_shape = input_shape[0:1] + torch.Size([x+2*y for x, y in zip(input_shape[-2:], padding)])
-        #if warns: warn('Do not forget to pad your input accoding to the Conv2d padding. Deactivate this warning passing warns=False as argument.', stacklevel=2)
-    
-    Cin, Hin, Win = input_shape
-    Cout = weight.shape[0]
-    Hk = weight.shape[2]
-    Wk = weight.shape[3]
-                                                                                  
-    #print('weights: ', weight)
-    uw = weight.flatten(start_dim=1, end_dim=-1)
-    #print('unrolled weights: ', uw)
+            raise RuntimeError('This functions does not account for dilation, if you extendent it, please send us a PR ;).')
 
-    return uw
-
-def unroll_image(img, kh, kw, ph, pw, pad_mode):
     ns = img.shape[0]
     nc = img.shape[1]
     ih = img.shape[2]
     iw = img.shape[3]
+    
+    nco, nci = weight.shape[0], weight.shape[1]
 
-    if pad_mode == 'zeros':
-        pad_mode = 'constant'
+    # kernel offsets
+    kh, kw = weight.shape[2], weight.shape[3]
+    ph, pw = padding
+    sh, sw = stride
+    dh, dw = dilation
+
+    oh = int(floor((ih - dh*(kh - 1) -1)/sh + 1))
+    ow = int(floor((iw - dw*(kw - 1) -1)/sw + 1))
+    print('out shape: ', oh, ow)
+
+    koht = floor(kh/2) 
+    kohb = kh - floor(kh/2) - 1 
+    kowr = floor(kw/2) 
+    kowl = kw - floor(kw/2) - 1
+    print('kernel offsets: ', koht, kohb, kowr, kowl)
+
+    assert nc == nci, f'Number of channels in the input image {nc} does not match with the conv layer {nci}'
+    
+    # pad image
+    pad_mode = pad_mode if pad_mode != 'zeros' else 'constant'
     ip = pad(img, pad=_reverse_repeat_tuple((ph, pw), 2), mode=pad_mode) 
-    #print('img pad:\n', ip)
+    print('img pad:\n', ip)
+    
+    ui = torch.zeros(ns, nci*kh*kw, oh*ow)
+    for _ho, _h in enumerate(range(koht, ih-kohb)):
+        for _wo, _w in enumerate(range(kowl, iw-kowr)):
+            print('limits: ', max(_h-koht, 0), min(_h+kohb+1, ih), max(_w-kowl,0), min(_w+kowr+1, iw))
+            oip = ip[:,:, max(_h-koht, 0):min(_h+kohb+1, ih), max(_w-kowl,0):min(_w+kowr+1, iw)]
+            print('oip: ', oip)
+            oipf = oip.flatten(start_dim=1, end_dim=-1)
+            print('oipf: ', oipf)
+            ui[:,:, _ho*ow+_wo] = oipf
+    print(ui)
 
+    '''
     # flattens the padded image
     ipf = ip.reshape(ns, nc, (ih+2*ph)*(iw+2*pw))
     print('ipf:\n', ipf)
 
     # repeats it for each kernel dimension
     ipfr = ipf.repeat(1, 1, kh*kw).reshape(ns, nc, kh*kw, (ih+2*ph)*(iw+2*pw))
+
     # rotates the image repetitions to align then wirh the kernel values
-    #base_shift = iw+2*pw+1
-    #print('base shift: ', base_shift)
+    base_shift = kohb*(iw+2*pw)+kowl 
+    print('base shift: ', base_shift)
+
     for _h in range(kh):
         for _w in range(kw):
-            print(2*kw*_w+2*kh*_h)
-            ipfr[:,:, _w+_h*kw] = ipfr[:,:, _w+_h*kw].roll(-(_w+_h*(iw+2*pw)))
-
-    return ipfr
+            ipfr[:,:, _w+_h*kw] = ipfr[:,:, _w+_h*kw].roll(base_shift-(_w+_h*(iw+2*pw)))
+    '''
+    return ui, oh, ow 
 
 
 if __name__ == '__main__':
@@ -83,8 +110,8 @@ if __name__ == '__main__':
         channel_wise = True 
         groups = 1#ri(1, 3)
         nc = 1#ri(2, 5)*groups # multiple of groups
-        kw = 3#ri(1, 2) # kernel width 
-        kh = 3#ri(1, 2) # kernel height
+        kw = 2#ri(1, 2) # kernel width 
+        kh = 2#ri(1, 2) # kernel height
         iw = 3#ri(2, 5) # image width
         ih = 3#ri(2, 5) # image height
         ns = 1 # n samples
@@ -119,17 +146,17 @@ if __name__ == '__main__':
             xu = x_pad.flatten().to(device)
 
         # get the sparse representation
-        my_csr = channel_conv(x[0].shape, c, device=device)
+        my_csr = channel_conv(c)
         print('unrolled kernel: ', my_csr)
         
         print('img:\n', x)
-        u_x_pad = unroll_image(x, kh, kw, ph, pw, pad_mode=c.padding_mode)
+        u_x_pad, oh, ow = unroll_image(x, c)
         print('uxpad:\n', u_x_pad, u_x_pad.shape, ih, iw, ph, pw)
         print('csr shape: ', my_csr.shape)
 
-        rr = (my_csr@u_x_pad).reshape(coc, ih+2*ph, iw+2*pw)
-        print('rr:\n', rr)
         print('r:\n', r)
+        rr = (my_csr@u_x_pad).reshape(ns, coc, oh, ow)
+        print('rr:\n', rr)
 
         quit()
         
