4bcf6cf464ad7525f54f8ca6a3e39f4d63edc21e
Leandro <leandro.desouzarosa@unibo.it>
2025-07-15 13:38:34 +0200

    oops

--- peepholelib/utils/analyze.py
+++ peepholelib/utils/analyze.py
@@ -30,6 +30,134 @@ def compute_top_k_accuracy(peepholes, targets, k):
     correct = (topk == targets).any(dim=1).float()
     return correct.mean().item()
 
+def conceptogram_entropy_score(**kwargs):
+    '''
+    Compute the Entropy score of all conceptograms in `phs._phs[`loaders`]`. `target_modules` are passed to `ph.get_conceptograms()` so the evaluation only consider the indicated modules. The score is computed as the entropy (`torch.distributions.Categorical.entropy()`) of the histogram of the conceptograms into `bins` bins.
+    If `plot=True` is passed, saves a KDE plot of the score and model confidence (`torch.nn.softmax(<model output>)`) for the correct and misclassified samples. `<model output>` is taken from `cvs`; it also plots the accuracy of the model dropping percentages of the dataset. 
+
+    Args:
+    - phs (peepholelib.peepholes.Peepholes): peepholes from which to take the conceptograms.
+    - cvs (peepholelib.coreVectors.CoreVectors): corevectors respective to the `phs`.
+    - loaders (list[str]): loaders to consider, usually ['train', 'test', 'val'].
+    - target_modules (list[str]): list if target modules, as keys from the model `statedict`.
+    - bins (int): number of bins to construct the histogram of conceptoframs. Weakly affects the score.
+    - plot (bool): save figure with distributions of correctly and miss-classified samples.
+    - max_drop (int): Max dataset drop for the accuracy plot.
+    - verbose (bool): print progress messages.
+
+    Returns:
+    - ret (dict('score': dict(), 'auc':dict())): entropy scores and AUC for each values in `loaders`.  
+    '''
+
+    phs = kwargs.get('peepholes')
+    cvs = kwargs.get('corevectors')
+    loaders = kwargs.get('loaders', ['test'])
+    target_modules = kwargs.get('target_modules', None)
+    bins = kwargs.get('bins', 50)
+    plot = kwargs.get('plot', False)
+    drop_max = kwargs.get('max_drop', 20)
+    verbose = kwargs.get('verbose', False)
+
+    score_name = 'Entropy'
+
+    # get conceptogram 
+    cpss = phs.get_conceptograms(loaders=loaders, target_modules=target_modules, verbose=verbose)
+
+    if plot:
+        fig, axs = plt.subplots(2, len(loaders), sharex='row', sharey='row', figsize=(4*len(loaders), 4))
+
+    # sizes just to facilitate 
+    nd = cpss[loaders[0]].shape[1] # number of layers (distributions)
+    nc = cpss[loaders[0]].shape[2] # number of classes
+
+    # for normalization
+    _line = torch.zeros(bins)
+    _line[0] = 1
+    min_e = Categorical(probs=_line).entropy()
+    _unif = torch.ones(bins)/bins
+    max_e = Categorical(probs=_unif).entropy()
+
+    # prepare returns dict
+    ret = {
+        'score': {}, 
+        'auc': {},
+    }
+
+    for loader_n, ds_key in enumerate(loaders):
+        cps = cpss[ds_key]
+        ns = cps.shape[0] # number of samples
+        results = cvs._dss[ds_key]['result']
+        confs = sm(cvs._dss[ds_key]['output'], dim=-1).max(dim=-1).values
+        
+        # main computation
+        scores = torch.zeros(ns)
+        for i in range(ns):
+            v = torch.histogram(cps[i], bins).hist
+            s = Categorical(probs=v).entropy()
+            scores[i] = 1-(s-min_e)/(max_e-min_e)
+
+        # compute AUC for score
+        s_auc = AUC().update(scores, results.int()).compute().item()
+
+        # Save returns
+        ret['score'][ds_key] = scores 
+        ret['auc'][ds_key] = s_auc
+
+        if verbose: print(f'AUC for {ds_key} split: {s_auc:.4f}')
+        
+        # plotting
+        if plot:
+            s_oks = scores[results == True]
+            s_kos = scores[results == False]
+            m_oks = confs[results == True]
+            m_kos = confs[results == False]
+
+            # compute AUC for model 
+            m_auc = AUC().update(confs, results.int()).compute().item()
+
+            df = pd.DataFrame({
+                'Value': torch.hstack((s_oks, s_kos, m_oks, m_kos)),
+                'Score': \
+                        [score_name+': OK' for i in range(len(s_oks))] + \
+                        [score_name+': KO' for i in range(len(s_kos))] + \
+                        ['Model: OK' for i in range(len(m_oks))] + \
+                        ['Model: KO' for i in range(len(m_kos))]
+                                                                                              
+                })
+            colors = ['xkcd:cobalt', 'xkcd:cobalt', 'xkcd:bluish green', 'xkcd:bluish green']
+
+            # effective plotting
+            ax = axs[0][loader_n] 
+            p = sb.kdeplot(
+                    data = df,
+                    ax = ax,
+                    x = 'Value',
+                    hue = 'Score',
+                    common_norm = False,
+                    palette = colors,
+                    clip = [0., 1.],
+                    alpha = 0.8,
+                    legend = loader_n == 0,
+                    )
+
+            lines = ['--', '-', '--', '-']
+            # set up linestyles
+            for ls, line in zip(lines, p.lines):
+                line.set_linestyle(ls)
+            
+            # set legend linestyle
+            if loader_n == 0:
+                handles = p.legend_.legend_handles[::-1]
+                for ls, h in zip(lines, handles):
+                    h.set_ls(ls)
+
+            ax.set_xlabel('Score')
+            ax.set_ylabel('%')
+            ax.title.set_text(f'{ds_key}\n{score_name} AUC={s_auc:.4f}\nModel AUC={m_auc:.4f}')
+            # plot dropping-out accuracy plot
+            _, s_idx = scores.sort()
+            _, m_idx = confs.sort()
+            s_acc = torch.zeros(drop_max+1)
             m_acc = torch.zeros(drop_max+1)
             for drop_perc in range(drop_max+1):
                 n_drop = floor((drop_perc/100)*ns)
