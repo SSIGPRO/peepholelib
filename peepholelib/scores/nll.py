103daae6eca097f98a571e28c16d07da6d875b09
clrnb <clara.barrancos@hotmail.com>
2025-11-12 15:20:34 +0100

    final nll i think

--- peepholelib/scores/nll.py
+++ peepholelib/scores/nll.py
@@ -2,17 +2,15 @@ def nll_score(**kwargs):
     '''
     Compute Negative Log-Likelihood (NLL) scores using a provided driller model.
     Args:
-    - datasets (peepholelib.datasets.parsedDataset.ParsedDataset): Parsed datasets.
     - loaders (list[str]): loaders like ['train', 'test', 'val']
-    - signal_key (str): 'residual' (error) or 'latent_space'
-    - driller (peepholelib.peepholes.drill_base.DrillBase): Driller model to compute NLL scores.
+    - driller: already loaded driller
+    - corevectors: already loaded corevectors
     - append_scores (dict): Append the scores form this dictionaty to the scores computed in this function.
     - score_name (str): name of the score to be stored in the return dictionary.
     '''
-    dss = kwargs.get('datasets')
     loaders = kwargs.get('loaders', ['train', 'test', 'val'])
-    signal_key = kwargs.get('signal_key', 'residual')  
     driller = kwargs.get('driller')
+    _cvs = kwargs.get('corevectors')
     append_scores = kwargs.get('append_scores', None)
     score_name = kwargs.get('score_name', 'nll')
 
@@ -26,16 +24,14 @@ def nll_score(**kwargs):
         if not ds_key in ret:
             ret[ds_key] = dict()
 
-
-    driller.load()
-    with driller as drl:
-        drl.score_samples()
-        for ds_key in loaders:
+    for ds_key in loaders:
             if ds_key not in ret:
                 ret[ds_key] = {}
-            
-            dss_flat = dss._dss[ds_key][signal_key].squeeze().flatten(start_dim=1)
+
+            cvs = _cvs._corevds[ds_key]
+            data = driller.parser(cvs=cvs)
+
             # get nll scores
-            ret[ds_key][score_name] = drl.score_samples(dss_flat)
+            ret[ds_key][score_name] = driller._classifier.score_samples(data)
     
     return ret
