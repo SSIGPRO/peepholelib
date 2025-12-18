8f9bec20171d499035345b5cf338ee6b1a281dbc
lorenzo <lorenzo.capelli@outlook.com>
2025-12-18 11:46:28 +0100

    getting those superclasses

--- peepholelib/datasets/cifar100.py
+++ peepholelib/datasets/cifar100.py
@@ -7,10 +7,18 @@ from peepholelib.datasets.functional.transforms import vgg16_cifar100
 
 # torch stuff
 import torch
+from torchvision.datasets import CIFAR100
 from torch.utils.data import random_split
 
 # CIFAR from torchvision
 from torchvision import datasets
+
+class CIFAR100Custom(CIFAR100):
+    def __getitem__(self, index):
+        img, target = super().__getitem__(index)
+        supertarget = target // 5
+
+        return img, target, supertarget
 
 class Cifar100(DatasetWrap):
     def __init__(self, **kwargs):
@@ -46,7 +54,7 @@ class Cifar100(DatasetWrap):
         torch.manual_seed(seed)
 
         # Test dataset is loaded directly
-        test_dataset = datasets.CIFAR100(
+        test_dataset = CIFAR100Custom(
             root = self.path,
             train = False,
             transform = transform,
@@ -54,7 +62,7 @@ class Cifar100(DatasetWrap):
         )
         
         # train data will be splitted into training and validation
-        _train_data = datasets.CIFAR100( 
+        _train_data = CIFAR100Custom( 
             root = self.path,
             train = True,
             transform = None, #transform,
