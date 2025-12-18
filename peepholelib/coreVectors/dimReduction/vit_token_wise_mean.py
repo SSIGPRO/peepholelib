0211419486ebe1b2191f0d54d427140f4d4c23fc
Leandro de Souza Rosa <lsrosa@users.noreply.github.com>
2025-12-18 19:37:08 +0100

    Refacor dimentionality reduction into a class. (#121)
    * depracate use_s and channel_wise
    
    refactor linear svd
    
    refactor conv2d toepliz svd
    
    refactor conv2d kernel svd
    
    refactor avgPooling
    
    refactor vit linear svd
    
    refactor vit token dim reductions
    
    refactor peepholes and drillers
    
    remove driller's suffix
    
    remove peepholes key from phs
    
    * update releasenotes
    
    * clean ups from PR review
    
    ---------
    
    Co-authored-by: Leandro <leandro.desouzarosa@unibo.it>

---
+++ peepholelib/coreVectors/dimReduction/vit_token_wise_mean.py
@@ -0,0 +1,29 @@
+# torch stuff
+import torch
+
+# Our stuff
+from .dim_reduction_base import DimReductionBase as DRB 
+
+class ViTTokenWiseMean(DRB):
+    def __init__(self, **kwargs):
+        DRB.__init__(self, **kwargs)
+        
+        # just to asset that the layer is Conv2d
+        layer = kwargs['layer']
+        model = kwargs['model']
+        _layer = model._target_modules[layer]
+        if not isinstance(_layer, torch.nn.Linear):
+            raise RuntimeError("Only Linear is suported") 
+
+        return
+
+    def __call__(self, **kwargs):
+        """
+        Compute the channel-wise mean for ViT activations.
+        """
+        # Assuming the input is of shape (batch_size, num_tokens, num_channels)
+
+        act_data = kwargs['act_data'] 
+        act_data = act_data[:, 1:, :]  # Exclude the class token
+        cvs = torch.mean(act_data, dim=2)  # Mean across tokens
+        return cvs
