60110c9e983a959315b07687067bf213588a9979
clrnb <clara.barrancos@hotmail.com>
2026-02-02 12:36:32 +0100

    create + parse dataset

--- peepholelib/datasets/parsedDataset.py
+++ peepholelib/datasets/parsedDataset.py
@@ -2,6 +2,8 @@
 from pathlib import Path as Path
 from tqdm import tqdm
 from math import ceil
+import math
+
 
 # tensordict
 from tensordict import PersistentTensorDict
@@ -50,113 +52,135 @@ class ParsedDataset():
         ds_wrap = kwargs['ds_wrap']
         bs = kwargs.get('batch_size', 2**11)
         verbose = kwargs.get('verbose', False)
-        
+        ds_samplers = kwargs.get('ds_samplers', None)
+        overwrite = kwargs.get('overwrite', False)
+
         path.mkdir(parents=True, exist_ok=True)
-        cls_inst = cls(path = path)
+        cls_inst = cls(path=path)
         cls_inst._dss = {}
 
-        ds_wrap.__load_data__()
+        ds_wrap.__load_data__(verbose=verbose)
 
         for ds_key in ds_wrap.__dataset__:
-            file_path = cls_inst.path/('dss.'+ds_key)
-            n_samples = len(ds_wrap.__dataset__[ds_key])
-            
-            # check if PTD exists 
+            ds_in_src = ds_wrap.__dataset__[ds_key]
+
+            sampler = None
+            if ds_samplers is not None:
+                if ds_key in ds_samplers:
+                    sampler = ds_samplers[ds_key]
+                else:
+                    split = ds_key.split("-")[-1]
+                    if split in ds_samplers:
+                        sampler = ds_samplers[split]
+
+            if sampler is not None:
+                perc = None
+                if hasattr(sampler, "keywords") and sampler.keywords is not None:
+                    perc = sampler.keywords.get("perc", None)
+                if perc is None:
+                    raise ValueError(f"Sampler for {ds_key} must be a partial with perc=... (got {sampler})")
+
+                n_full = len(ds_in_src)
+                n_keep = max(1, int(math.ceil(n_full * float(perc))))
+
+                idx = torch.randperm(n_full)[:n_keep].tolist()
+                ds_in_src = torch.utils.data.Subset(ds_in_src, idx)
+
+                if verbose:
+                    print(f"Subsampling {ds_key}: keeping {n_keep}/{n_full} (~{perc})")
+
+            n_samples = len(ds_in_src)
+            file_path = cls_inst.path / ('dss.' + ds_key)
+
+            if overwrite and file_path.exists():
+                file_path.unlink()
+
             if file_path.exists():
-                if verbose: print(f'File {file_path} exists. Loading from disk.')
+                if verbose:
+                    print(f'File {file_path} exists. Loading from disk.')
                 cls_inst._dss[ds_key] = PersistentTensorDict.from_h5(file_path, mode='r+')
-
             else:
-                if verbose: print(f'Creating {ds_key} dataset with n_samples: ', n_samples)
+                if verbose:
+                    print(f'Creating {ds_key} dataset with n_samples: ', n_samples)
+
                 cls_inst._dss[ds_key] = PersistentTensorDict(filename=file_path, batch_size=[n_samples], mode='w')
-                
-                # get sample to get shapes
-                sample = ds_wrap.__dataset__[ds_key][0]
+
+                sample = ds_in_src[0]
+                if not isinstance(sample, dict):
+                    raise TypeError(f"Dataset {ds_key} must return a dict. Got {type(sample)}")
+
                 for key in sample.keys():
-                    if verbose: print(f'allocating {key} with shape {sample[key].shape}')
-                    cls_inst._dss[ds_key][key] = MMT.empty(shape=torch.Size((n_samples,)+sample[key].shape), dtype=sample[key].dtype)
-        
-                # create Dataloader of input dataset
-                ds_in = DataLoader(
-                    dataset = ds_wrap.__dataset__[ds_key],
-                    batch_size = bs
-                )
+                    v = sample[key]
+                    if not torch.is_tensor(v):
+                        raise TypeError(f"Key '{key}' in {ds_key} must be a torch.Tensor. Got {type(v)}")
 
-                ds_t = DataLoader(
-                    cls_inst._dss[ds_key],
-                    collate_fn = lambda x:x, 
-                    batch_size = bs
-                )
+                    if verbose:
+                        print(f'allocating {key} with shape {v.shape}')
+                    cls_inst._dss[ds_key][key] = MMT.empty(
+                        shape=torch.Size((n_samples,) + v.shape),
+                        dtype=v.dtype
+                    )
 
-                for data_in, data_t in tqdm(zip(ds_in, ds_t), disable=not verbose, total=ceil(n_samples/bs)):
+                ds_in = DataLoader(dataset=ds_in_src, batch_size=bs)
+                ds_t = DataLoader(cls_inst._dss[ds_key], collate_fn=lambda x: x, batch_size=bs)
+
+                for data_in, data_t in tqdm(zip(ds_in, ds_t), disable=not verbose, total=ceil(n_samples / bs)):
                     for key in data_in.keys():
                         data_t[key] = data_in[key]
-            
-            # close the PTD
-            cls_inst._dss[ds_key].close()
-        return cls_inst
-    
-    def parse_ds(self, **kwargs):
-        #self.check_uncontexted()
 
+            cls_inst._dss[ds_key].close()
+
+        return cls_inst
+
+
+    def parse_ds(self, **kwargs):
         model = kwargs['model']
         loaders = kwargs.get('loaders', None)
         bs = kwargs.get('batch_size', 2**11)
         verbose = kwargs.get('verbose', False)
-        ds_samplers = kwargs.get('ds_samplers', None)
         pred_fn = kwargs.get('pred_fn', multilabel_classification)
         result_fn = kwargs.get('result_fn', results_one_hot_encoding)
-        
-        if loaders == None:
+
+        if loaders is None:
             loaders = self._dss.keys()
 
         for ds_key in loaders:
+            file_path = self.path / ('dss.' + ds_key)
+            self._dss[ds_key] = PersistentTensorDict.from_h5(file_path, mode='r+')
+
             n_samples = len(self._dss[ds_key])
 
-            if ds_samplers is not None and ds_key in ds_samplers:
-                if verbose:
-                    print(f'Applying {ds_samplers[ds_key]} to {ds_key}')
-                ds_samplers[ds_key](ds=self._dss[ds_key])
-            
-            # dataset sample for dry run
             sample = self._dss[ds_key][0:1]['image'].to(model.device)
             with torch.no_grad():
-                _out, _ls = model(sample)
-                     
-            os = _out.shape[1:]
-            ls = _ls.shape[1:]
+                out = model(sample)
+            _out = out[0] if isinstance(out, (tuple, list)) else out
 
-            # check and skip if the values are already there
             if ('output' in self._dss[ds_key]) and ('pred' in self._dss[ds_key]) and ('result' in self._dss[ds_key]):
                 continue
 
-            # need to fix the batch size - workaround  
             self._dss[ds_key].batch_size = torch.Size((n_samples,))
-            # allocate disk space
-            num_classes =_out.shape[1]
-            print("number of classes: ", num_classes)
-            quit()
+            num_classes = _out.shape[1]
+
             self._dss[ds_key]['output'] = MMT.empty(shape=torch.Size((n_samples, num_classes)))
             self._dss[ds_key]['pred'] = MMT.empty(shape=torch.Size((n_samples,)))
-            self._dss[ds_key]['result'] = MMT.empty(shape=torch.Size((n_samples,))) 
+            self._dss[ds_key]['result'] = MMT.empty(shape=torch.Size((n_samples,)))
 
-            dl = DataLoader(
-                self._dss[ds_key],
-                collate_fn = lambda x:x, 
-                batch_size = bs
-            )
+            dl = DataLoader(self._dss[ds_key], collate_fn=lambda x: x, batch_size=bs)
 
-            for data in tqdm(dl, disable=not verbose, total=ceil(n_samples/bs)):
-                #compute predictions which is the out of decoder
+            for data in tqdm(dl, disable=not verbose, total=ceil(n_samples / bs)):
                 with torch.no_grad():
-                    y_predicted = model(data['image'].to(model.device))
-            
-                    predicted_labels = pred_fn(y_predicted).detach().cpu()
-                    data['output'] = y_predicted
-                    data['pred'] = predicted_labels
-                    data['result'] = result_fn(predicted_labels, data['label'])         
-        
+                    y = model(data['image'].to(model.device))
+                y_predicted = y[0] if isinstance(y, (tuple, list)) else y
+
+                predicted_labels = pred_fn(y_predicted).detach().cpu()
+                data['output'] = y_predicted
+                data['pred'] = predicted_labels
+                data['result'] = result_fn(predicted_labels, data['label'])
+
+            self._dss[ds_key].close()
+
         return self
+
     
     
     def load_only(self, **kwargs):
