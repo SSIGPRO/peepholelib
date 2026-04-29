80794b32f71dfbb8594e645dd272957c54078a7b
Leandro <leandro.desouzarosa@unibo.it>
2026-04-29 16:13:40 +0200

    add MNIST and textures

--- peepholelib/datasets/MNIST.py
+++ peepholelib/datasets/MNIST.py
@@ -5,14 +5,13 @@ from peepholelib.datasets.datasetWrap import DatasetWrap
 import torch
 from torch.utils.data import random_split
 
-# Places365 from torchvision
-from torchvision.datasets import Places365 as torchPlaces
+# MNIST from torchvision
+from torchvision.datasets import MNIST as torchMNIST
 from torchvision.transforms import ToTensor
 
-class PlacesCustom(torchPlaces):
-
+class MNISTCustom(torchMNIST):
     def __init__(self, **kwargs):
-        torchPlaces.__init__(self, **kwargs)
+        torchMNIST.__init__(self, **kwargs)
 
     def __getitem__(self, index):
         img, label = super().__getitem__(index)
@@ -22,13 +21,13 @@ class PlacesCustom(torchPlaces):
                 'label': torch.tensor(label),
                 }  
 
-class Places(DatasetWrap):
+class MNIST(DatasetWrap):
     def __init__(self, **kwargs):
         '''
-        Places loader (val & test). Validation is created from train.
+        MNist loader (val & test). Validation is created from train.
 
         Expects:
-            path (str): Places download folder. If not downloaded, downloads the dataset in this folder.
+            path (str): MNIST download folder. If not downloaded, downloads the dataset in this folder.
         Returns:
             - a thumbs up
         '''
@@ -37,7 +36,7 @@ class Places(DatasetWrap):
         # add a default transform for specific DS
         self.transform = kwargs.get('std_transfrom', None)
 
-        self.splitting_ratio = kwargs.get('splitting_ratio', [0.45205478, 0.27397261, 0.27397261])
+        self.splitting_ratio = kwargs.get('splitting_ratio', [5/6, 1/6])
 
         # append ToTensor to the transform
         if self.transform != None:
@@ -49,30 +48,37 @@ class Places(DatasetWrap):
     
     def __load_data__(self):
         '''
-        Load and prepare Places data.
+        Load and prepare MNIST data.
         
         Returns:
         - a thumbs up
         '''
 
-        _data = PlacesCustom(
+        _train_data = MNISTCustom(
                 root = self.path,
-                split = 'val',
+                train = True,
                 transform = self.transform,
-                small = True,
                 download = True
                 )
 
         # split to get 10000 samples for val and test
-        _, val_dataset, test_dataset = random_split(
-                _data,
+        _, val_dataset = random_split(
+                _train_data,
                 self.splitting_ratio, 
                 generator=torch.Generator().manual_seed(self.seed)
                 )
+
+        # this one has 10000 samples for test
+        test_dataset = MNISTCustom(
+                root = self.path,
+                train = False,
+                transform = self.transform,
+                download = True
+                )
                     
         self.__dataset__ = {
-                'Places365-val': val_dataset,
-                'Places365-test': test_dataset
+                'MNIST-val': val_dataset,
+                'MNIST-test': test_dataset
                 }
-        
+
         return
