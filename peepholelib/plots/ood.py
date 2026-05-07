9620f190329c50cb6f978bbc4d901e436a3f8d51
Leandro <leandro.desouzarosa@unibo.it>
2026-05-07 17:21:23 +0200

    fixes

--- peepholelib/plots/ood.py
+++ peepholelib/plots/ood.py
@@ -67,8 +67,8 @@ def plot_ood(**kwargs):
 
             # guarantees the same number of samples
             _ns = min(len(s_id), len(s_ood))
-            s_id = s_ori[torch.randperm(len(s_id))[:_ns]]
-            s_ood = s_atk[torch.randperm(len(s_ood))[:_ns]]
+            s_id = s_id[torch.randperm(len(s_id))[:_ns]]
+            s_ood = s_ood[torch.randperm(len(s_ood))[:_ns]]
 
             # computing AUC for each score type
             _labels = torch.hstack((torch.ones(s_id.shape), torch.zeros(s_ood.shape)))
