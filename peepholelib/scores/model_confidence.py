8ef599e725c8e174272ea785c3d7a9b91e600244
Lorenzo Capelli <lorenzo99capelli@gmail.com>
2026-09-09 11:02:48 +0200

    update

--- peepholelib/scores/model_confidence.py
+++ peepholelib/scores/model_confidence.py
@@ -1,25 +1,8 @@
 from torch.nn.functional import softmax as sm
 from peepholelib.scores.score import Score
 
-
-    Args:
-    - datasets (peepholelib.datasets.parsedDataset.ParsedDataset): parsed dataset.
-    - loaders (list[str]): loaders to consider, usually ['train', 'test', 'val'], if 'None', gets all loaders in 'corevectors._dss'. Defaults to 'None'.
-    - append_scores (dict): Append the scores in this dictionaty to the scores computed in this function. Overwrite if same keys.
-    - verbose (bool): print progress messages.
-
-    Returns:
-    - ret (dict(str:dict(str:torch.tensor))): Scores as a two level dictionaty with the first key being the loaders, and second being the score name 'Model-Confidence'. If 'append_scores' is passed, the dictionaries are appended.
-    '''
-
-    dss = kwargs.get('datasets')
-    loaders = kwargs.get('loaders', None)
-    append_scores = kwargs.get('append_scores', None)
-    score_name = kwargs.get('score_name', 'MSP')
-    verbose = kwargs.get('verbose', False)
-    
-    # parse arguments
-    if loaders == None: loaders = list(dss._dss.keys())
+class MSPScore(Score):
+    score_name = 'MSP'
 
     def _compute(self, **kwargs):
         dss = kwargs['datasets']
